9f50bab33b8bd02a0e7f83339364b639c6687bac
Wrafttex <nkure19@student.aau.dk>
2022-11-21 11:51:20 +0100

    Publish and Subscribe to a docker hosted broker

---
+++ mqttTools/subscribe.py
@@ -0,0 +1,37 @@
+from paho.mqtt import client as mqtt_client
+
+broker = "localhost"
+port = 1883
+topic = "test/topic"
+username = "TestUser"
+password = "TestPassword"
+clientID = "HubSubcribe"
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(clientID)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        return msg.payload.decode()
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+if __name__ == '__main__':
+    run()
